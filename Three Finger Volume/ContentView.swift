54e2385a29ccca29632e47e4ab7bd75d30dd371e
axolotl-industries <geoff@cuthbertevans.com>
2026-04-18 17:03:04 +0100

    Initial Commit

---
+++ Three Finger Volume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Three Finger Volume
+//
+//  Created by geoff on 18/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Three Finger Volume/Three_Finger_VolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Finger_VolumeApp.swift
+//  Three Finger Volume
+//
+//  Created by geoff on 18/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Three_Finger_VolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
